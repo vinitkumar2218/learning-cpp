0842ca92982997c15577f94d6e0cb6b0b78933af
vinitkumar2218 <vinitkumar18914119@gmail.com>
2026-03-24 21:04:32 +0530

    Create division_of_numbers.cpp
    "This is my seventh program in C++: Division of numbers by user input."

---
+++ division_of_numbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  float x,y;
+  cout<<"enter the numbers: \n";
+  cout<<"first number: ";
+  cin>>x;
+  cout<<"second number: ";
+  cin>>y;
+  cout<<"division of the number is: "<<x/y;
+  return 0;
+}
