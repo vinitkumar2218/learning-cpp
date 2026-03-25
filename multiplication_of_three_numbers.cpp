0caaf03aaf28a49ebf237497eaa885d4560cc2f1
vinitkumar2218 <vinitkumar18914119@gmail.com>
2026-03-25 09:35:15 +0530

    Create multiplication_of_three_numbers.cpp
    "This is my ninth program in C++: multiplication of three numbers."

---
+++ multiplication_of_three_numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"enter the numbers:\n";
+  float x,y,z;
+  cout<<"first number: ";
+  cin>>x;
+  cout<<"second number: ";
+  cin>>y;
+  cout<<"third number: ";
+  cin>>z;
+  cout<<"multiplication of the numbers is: "<<x*y*z<<endl;
+  return 0;
+}
