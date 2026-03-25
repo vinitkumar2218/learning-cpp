f72603fe227b01b47873e0af9dde6b57f821ed4e
vinitkumar2218 <vinitkumar18914119@gmail.com>
2026-03-25 22:23:48 +0530

    Create check_numbers_even_or_odd.cpp
    "This is my ninth program in C++: checking numbers even or odd."

---
+++ check_numbers_even_or_odd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"enter the number:\n";
+  cin>>n;
+  if (n % 2 == 0)
+    cout<< n <<" is an even number:\n";
+  else
+    cout<< n <<" is an odd number:\n";
+  return 0;
+}
